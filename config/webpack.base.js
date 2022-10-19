cac9ca177011d6a5a3c3cffa6aead76e17a84cd4
harrison <851596652@qq.com>
2022-10-19 20:53:33 +0800

    babel copy-plugin

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
-console.log('NODE_ENV', process.env.NODE_ENV)
-console.log('BASE_ENV', process.env.BASE_ENV)
+console.log("NODE_ENV", process.env.NODE_ENV);
+console.log("BASE_ENV", process.env.BASE_ENV);
 
 module.exports = {
   entry: path.join(__dirname, "../src/index.tsx"), // 入口文件
@@ -17,29 +17,45 @@ module.exports = {
     rules: [
       {
         test: /.(ts|tsx)$/, // 匹配ts和tsx
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-typescript"],
-          },
-        },
+        use: 'babel-loader'
+        // use: {
+        //   loader: "babel-loader",
+        //   options: {
+        //     presets: [
+        //       [
+        //         "@babel/preset-env",
+        //         {
+        //           // 设置兼容目标浏览器版本,这里可以不写,babel-loader会自动寻找上面配置好的文件.browserslistrc
+        //           // "targets": {
+        //           //  "chrome": 35,
+        //           //  "ie": 9
+        //           // },
+        //           "useBuiltIns": "usage", // 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
+        //           "corejs": 3, // 配置使用core-js低版本
+        //         },
+        //       ],
+        //       "@babel/preset-react",
+        //       "@babel/preset-typescript",
+        //     ],
+        //   },
+        // },
       },
       {
         test: /.(css|less)$/,
-        use: ['style-loader', 'css-loader', 'less-loader']
-      }
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      },
     ],
   },
   resolve: {
-    extensions: ['.js', '.tsx', '.ts']
+    extensions: [".js", ".tsx", ".ts"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'),
+      template: path.resolve(__dirname, "../public/index.html"),
       inject: true, // 自动注入静态资源
     }),
     new webpack.DefinePlugin({
-      'process.env.BASE_ENV': JSON.stringify(process.env.BASE_ENV)
-    })
-  ]
+      "process.env.BASE_ENV": JSON.stringify(process.env.BASE_ENV),
+    }),
+  ],
 };
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,18 @@
-const {merge} = require('webpack-merge')
-const baseConfig = require('./webpack.base.js')
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(baseConfig, {
-    mode: 'production', // 会开启tree-shaking和压缩代码等优化
-})
+  mode: "production", // 会开启tree-shaking和压缩代码等优化
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "../public"), // 将public下文件
+          to: path.resolve(__dirname, "../dist"), // 复制到dist目录
+          filter: (source) => !source.includes("index.html"), // 忽略index.html
+        },
+      ],
+    }),
+  ],
+});
