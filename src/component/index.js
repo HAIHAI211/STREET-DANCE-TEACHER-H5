7d9fa80d45f0c5739021a8c236d4cbf85f8c5df9
harrison <851596652@qq.com>
2022-10-19 11:21:28 +0800

    init

--- src/component/index.js
+++
@@ -1,5 +0,0 @@
-import Loading from "./Loading";
-import CalendarHeader from "./CalendarHeader";
-import CalendarBody from "./CalendarBody";
-import BottomNavigation from "./BottomNavigation";
-export { Loading, CalendarHeader, CalendarBody, BottomNavigation };
--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { legacy_createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import reducer from "./reducers";
-import mySaga from "../models";
-
-const sagaMiddleware = createSagaMiddleware();
-const index = legacy_createStore(reducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(mySaga);
-export default index;
