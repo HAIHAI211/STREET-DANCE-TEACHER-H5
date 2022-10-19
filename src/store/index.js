66fe126b3ba6883ca09e9399b1b6c70ad9411219
harrison <851596652@qq.com>
2022-10-19 10:31:44 +0800

    rename models => saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { legacy_createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import reducer from "./reducers";
-import mySaga from "../models";
+import mySaga from "../saga";
 
 const sagaMiddleware = createSagaMiddleware();
 const index = legacy_createStore(reducer, applyMiddleware(sagaMiddleware));
