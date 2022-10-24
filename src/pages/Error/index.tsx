b9461a7a79ab1b6702f40fad2d65648ce572d10b
harrison <851596652@qq.com>
2022-10-24 21:35:29 +0800

    errorPage

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { useRouteError } from "react-router-dom";
+
+export const ErrorPage: FC<any> = (props) => {
+  const error: any = useRouteError();
+  console.log(error)
+  return (
+    <div className="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import { BaseLayout } from "@/layout";
 import { HomePage } from "@/pages/Home";
+import { ErrorPage } from "@/pages/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <BaseLayout />,
+    errorElement: <ErrorPage />,
   },
 ]);
